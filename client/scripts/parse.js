85cd1aeca33d700a3650dcf00931586f945b4ba0
Phillip Alexander <phillip.alexander@galvanize.com>
2021-03-17 12:30:03 +0000

    (init)

---
+++ client/scripts/parse.js
@@ -0,0 +1,22 @@
+var Parse = {
+
+  server: `https://app-hrsei-api.herokuapp.com/api/chatterbox/messages/${window.CAMPUS}`,
+
+  create: function(message, successCB, errorCB = null) {
+    // todo: save a message to the server
+  },
+
+  readAll: function(successCB, errorCB = null) {
+    $.ajax({
+      url: Parse.server,
+      type: 'GET',
+      data: { order: '-createdAt' },
+      contentType: 'application/json',
+      success: successCB,
+      error: errorCB || function(error) {
+        console.error('chatterbox: Failed to fetch messages', error);
+      }
+    });
+  }
+
+};
