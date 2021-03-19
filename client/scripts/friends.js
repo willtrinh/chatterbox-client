65c55cdccc280995ef474d48f75b9e4d30834e02
Will Trinh <williamltrinh@gmail.com>
2021-03-19 16:30:56 -0700

    Implemented friends toggleStatus

--- client/scripts/friends.js
+++ client/scripts/friends.js
@@ -1,4 +1,12 @@
 var Friends = {
+  _friendsList: new Set(),
 
+  toggleStatus: function(friend) {
+    if (!Friends._friendsList.has(friend)) {
+      Friends._friendsList.add(friend);
+    }
+
+    MessagesView.render();
+  }
 
 };
