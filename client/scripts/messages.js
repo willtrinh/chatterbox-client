4a1a0f47601410724a29018da2311a6423be33ad
Will Trinh <williamltrinh@gmail.com>
2021-03-19 16:31:35 -0700

    Implemented messages view and model

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -1,4 +1,9 @@
 var Messages = {
+  _storage: {},
 
+  // return all the message in the storage object
+  get: function() {
+    return _.chain(Object.values(Messages._storage)).sortBy('createdAt');
+  }
 
 };
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,9 +3,30 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
+    MessagesView.$chats.on('click', '.username', function(event) {
+      console.log(event);
+      var username = event.currentTarget.textContent;
+      Friends.toggleStatus(username);
+    });
   },
 
   render: function() {
+    // iterate through each message object
+    // create new messageView for each message with corresponding roomname
+    // render
+    MessagesView.$chats.html('');
+    // if (Rooms.getSelectedRoom() === '') {
+    //   Messages.get().each(message => MessagesView.renderMessage(message));
+    // } else {
+    //   Messages.get()
+    //     .filter(messsage => message.roomname === Rooms.getSelectedRoom())
+    //     .each(message => MessagesView.renderMessage(message));
+    // }
+
+  },
+  renderMessage: function(message) {
+    var $message = MessageView.render(message);
+    MessagesView.$chats.prepend($message);
   }
 
 };
