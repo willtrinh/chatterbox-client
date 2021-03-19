ebc820f509b93360459570d68dd012696cfe6ed0
Will Trinh <williamltrinh@gmail.com>
2021-03-19 16:32:46 -0700

    Implemented room view and model

--- client/scripts/rooms.js
+++ client/scripts/rooms.js
@@ -1,4 +1,36 @@
 var Rooms = {
+  // Each room name needs to be unique -> use Set
+  _id: new Set(),
+  _selectedRoom: '',
 
+  get: function() {
+    return [...Rooms._id.values()];
+  },
+
+  add: function(room) {
+    Rooms._id.add(room);
+    Rooms.setSelectedRoom(room);
+
+    RoomsView.render();
+  },
+
+  update: function() {
+    // get messages that exist in all rooms
+    Messages.get().forEach(function(message) {
+      // add the message exists in that specific roomname
+      Rooms._id.add(message.roomname);
+    });
+    // render rooms list
+    RoomsView.render();
+  },
+
+  setSelectedRoom: function(room) {
+    Rooms._selectedRoom = room;
+    MessagesView.render();
+  },
+
+  getSelectedRoom: function() {
+    return Rooms._selectedRoom;
+  }
 
 };
--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -4,9 +4,26 @@ var RoomsView = {
   $select: $('#rooms select'),
 
   initialize: function() {
+    RoomsView.$button.on('click', function() {
+      var room = prompt('Please enter your room name');
+      Rooms.add(room);
+      RoomsView.$select.val(room);
+    });
+    RoomsView.$select.on('change', Rooms.setSelectedRoom(RoomsView.$select.val()));
   },
 
   render: function() {
+    // re-render the component
+    RoomsView.$select.html('');
+    var $rooms = Rooms.get();
+    _.each($rooms, function(room) {
+      RoomsView.renderRoom(room);
+    });
+  },
+
+  renderRoom: function(room) {
+    var $option = $('<option>').val(room).text(room);
+    RoomsView.$select.append($option);
   }
 
 };
